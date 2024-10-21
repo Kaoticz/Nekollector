40f324c0c7e4b94f0290951a6bb6d75ed62bd9aa
Kotz <1812311+Kaoticz@users.noreply.github.com>
2024-10-21 16:21:09 -0300

    Added sidebar and loading image (#2)

--- src/main/java/com/github/kaoticz/nekollector/MainApplication.java
+++ src/main/java/com/github/kaoticz/nekollector/MainApplication.java
@@ -7,12 +7,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        stage.setMinHeight(400);
+        stage.setMinWidth(500);
+        stage.setHeight(700);
+        stage.setWidth(800);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/github/kaoticz/nekollector/MainController.java
@@ -0,0 +1,45 @@
+package com.github.kaoticz.nekollector;
+
+import com.github.kaoticz.nekollector.common.Statics;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class MainController {
+
+    private static int favoriteCounter = 1;
+
+    @FXML
+    private VBox sideBarContainer;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    public void initialize() {
+        imageView.setImage(Statics.loadingImage);
+    }
+
+    @FXML
+    public void addFavoriteButton(ActionEvent ignoredEvent) {
+        var button = new Button("Favorite " + favoriteCounter++);
+        this.sideBarContainer.getChildren().add(button);
+    }
+
+    @FXML
+    public void moveToPreviousImage(ActionEvent ignoredEvent) {
+        System.out.println("moveToPreviousImage press!");
+    }
+
+    @FXML
+    public void moveToNextImage(ActionEvent ignoredEvent) {
+        System.out.println("moveToNextImage press!");
+    }
+
+    @FXML
+    public void downloadImage(ActionEvent ignoredEvent) {
+        System.out.println("downloadImage press!");
+    }
+}
