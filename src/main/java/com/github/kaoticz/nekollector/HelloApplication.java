424fb3c0023befee1f8304b91180a0c0fe05c83e
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2024-10-21 00:42:44 -0300

    Basic application layout

--- src/main/java/com/github/kaoticz/nekollector/HelloApplication.java
+++ src/main/java/com/github/kaoticz/nekollector/HelloApplication.java
@@ -13,6 +13,10 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        stage.setMinHeight(400);
+        stage.setMinWidth(500);
+        stage.setHeight(700);
+        stage.setWidth(800);
         stage.setScene(scene);
         stage.show();
     }
